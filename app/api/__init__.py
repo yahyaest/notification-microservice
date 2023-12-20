890fe90cca1b2f286e5f657591cee0509c5bc25d
yahya <yahyaest1000@gmail.com>
2023-12-20 06:03:43 +0100

    Implement bulk notification update

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from app.api.notification import router as notificationRouter
+from app.api.bulkNotification import router as bulkNotificationRouter
 
 api = APIRouter()
 api.include_router(notificationRouter)
+api.include_router(bulkNotificationRouter)
 
 __all__ = ["api"]
---
+++ app/api/bulkNotification.py
@@ -0,0 +1,45 @@
+import logging
+from typing import List, Optional
+from fastapi import APIRouter, HTTPException, Request
+from pydantic import BaseModel
+from app.prisma import prisma
+
+logger = logging.getLogger(__name__)
+router = APIRouter()
+
+class Notification(BaseModel):
+    userEmail: Optional[str] = None
+    userId: Optional[int] = None
+    username: str
+    userImage: Optional[str] = None
+    title: str
+    message: str
+    seen: Optional[bool] = False
+
+@router.patch("/bulk_notifications", tags=["notification"])
+async def update_bulk_notification(body: dict, request: Request):
+    try:
+        user = request.user
+
+        if not user:
+            raise HTTPException(status_code=401, detail="Unauthorized")
+
+        notifications = await prisma.notification.find_many(
+            where= { 'userEmail': user.get('email') }
+        )
+
+        if not notifications:
+            raise HTTPException(status_code=404, detail="Notifications not found")
+        
+        if user.get('email') != notifications[0].userEmail and user.get('role') != 'ADMIN':
+            raise HTTPException(status_code=403, detail="Notifications belong to another user")    
+
+        updatedNotifications = await prisma.notification.update_many(
+            where= { 'userEmail': user.get('email') },
+            data= dict(body)
+        )
+
+        return updatedNotifications
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=404, detail=e.detail)
