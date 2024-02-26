1056042b7bab371e26b705a90d5235e3d6ddfe65
yahya <yahyaest1000@gmail.com>
2024-02-26 14:59:19 +0100

    Export database url from docker compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,5 +11,12 @@
 prisma migrate dev
 prisma generate
 
+# Create or update .env file with provided environment variables
+
+echo "GATEWAY_BASE_URL=${GATEWAY_BASE_URL}" > /app/.env
+echo "LOG_LEVEL=${LOG_LEVEL}" >> /app/.env
+echo "DATABASE_URL=${DATABASE_URL}" >> /app/.env
+echo "JWT_SECRET=${JWT_SECRET}" >> /app/.env
+
 # Start the FastAPI application
 exec uvicorn app.main:app --host 0.0.0.0 --reload --port 8000
